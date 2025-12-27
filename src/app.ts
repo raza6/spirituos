6c85c5ffedbdca05de268187e491b8385f2fd58c
Antonin Gontier <gontier.antonin@orange.fr>
2025-12-27 02:03:00 +0100

    ✨ better tree display

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { engine } from 'express-handlebars';
+import Handlebars from 'handlebars';
 import path from 'path';
 import { getRandomSentence } from './services/tatoebaConnector';
 import { analyzeSentence } from './services/satzklarConnector';
+import { AnalysisComponent } from './types/analyzis';
 const app = express();
 const port = 3000;
 
@@ -11,6 +13,59 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'views'));
+
+Handlebars.registerPartial('sentencePart', 
+`
+  {{#noPunkt part.component}}
+    <div class="tree-branch">
+      <strong>
+        {{#soloWord part.word}}
+          <a class="deflink" href="https://en.wiktionary.org/wiki/{{uncapitalize part}}#German">{{part.word}}</a>
+        {{else}}
+          {{part.word}}
+        {{/soloWord}}
+      </strong>-
+      <em data-tooltip="{{part.description}}">{{part.component}}</em>
+      {{#sameChild part}}
+        <em data-tooltip="{{part.children.[0].description}}">{{part.children.[0].component}}</em>
+      {{else}}
+        {{#each part.children}}
+          {{>sentencePart part=.}}
+        {{/each}}
+      {{/sameChild}}
+    </div>
+  {{/noPunkt}}
+`);
+
+Handlebars.registerHelper('noPunkt', function (component: string, options) {
+  if (component !== 'Interpunktion') {
+    return options.fn(this);
+  }
+});
+
+Handlebars.registerHelper('sameChild', function(parent: AnalysisComponent, options) {
+  if (parent.children?.length === 1 && parent.word === parent.children?.[0]?.word) {
+    return options.fn(this);
+  } else {
+    return options.inverse(this);
+  }
+});
+
+Handlebars.registerHelper('soloWord', function(word: string, options) {
+  if (!word.includes(' ')) {
+    return options.fn(this);
+  } else {
+    return options.inverse(this);
+  }
+});
+
+Handlebars.registerHelper('uncapitalize', function(element: AnalysisComponent): string {
+  if (element.component !== 'Substantiv') {
+    return element.word[0].toLowerCase() + element.word.slice(1);
+  } else {
+    return element.word;
+  }
+});
 
 app.get('/', async (req, res) => {
   try {
@@ -35,6 +90,11 @@ app.get('/analyzis/:sentence', async (req, res) => {
   
     const analyzisCore = analyzis.data.output.component.analysis;
     const analyzisTree = analyzis.data.output.component.tree;
+
+    const references = flattenToReferences(analyzisTree.children)
+    .filter((v, i, self) => {
+      return self.findIndex(vv => vv.type === v.type) === i;
+    });
   
     res.render('analyzis', {
       layout: false,
@@ -43,29 +103,8 @@ app.get('/analyzis/:sentence', async (req, res) => {
         wordOrder: analyzisCore.wordOrder,
         caseUsage: analyzisCore.caseUsage,
         explanation: analyzisCore.pragmatics.explanation,
-        tree: analyzisTree.children
-          .filter(c => c.component !== 'Interpunktion')
-          .map(c => {
-            if (c.word === c.children?.[0]?.word) { // flatten
-              return {
-                word: c.word,
-                detail: [
-                  {
-                    type: c.component,
-                    description: c.description
-                  },
-                  {
-                    type: c.children[0].component,
-                    description: c.children[0].description
-                  }
-                ]
-              }
-            }
-            return { // remap
-              ...treeComponentMapper(c),
-              children: c.children?.map(treeComponentMapper)
-            }
-          })
+        tree: analyzisTree.children,
+        references
       }
     });
   } catch {
@@ -73,16 +112,47 @@ app.get('/analyzis/:sentence', async (req, res) => {
   }
 });
 
-function treeComponentMapper(component) {
-  return {
-    word: component.word,
-    detail: [
-      {
-        type: component.component,
-        description: component.description
+function flattenToReferences(tree: AnalysisComponent[], ref = []): {type: string, ref: string}[] {
+  for (const element of tree) {
+    const currentRef = matchWithReference(element.component);
+    if (currentRef) {
+      ref = [...ref, { type: element.component, ref: currentRef }]  
+    }
+    if (element.children) {
+      const childRef = flattenToReferences(element.children);
+      if (childRef) {
+        ref = [...ref, ...childRef];
       }
-    ]
-  };
+    }
+  }
+
+  return ref;
+}
+
+function matchWithReference(component: string): string {
+  switch (component) {
+    case 'Verb':
+      return 'https://en.wikibooks.org/wiki/German/Grammar/Verbs';
+    case 'Koordination':
+      return 'https://en.wikibooks.org/wiki/German/Grammar/Coordinating_conjunctions';
+    case 'Konjunktion':
+      return 'https://en.wikibooks.org/wiki/German/Grammar/Subordinating_conjunctions';
+    case 'Infinitiv':
+    case 'Infinitivphrase':
+      return 'https://en.wikibooks.org/wiki/German/Grammar/Sentences#Infinitive_Clauses';
+    case 'Modalverb':
+      return 'https://en.wikibooks.org/wiki/German/Grammar/Modal_auxiliary_verbs';
+    case 'Pronomen':
+      return 'https://en.wikibooks.org/wiki/German/Grammar/Personal_pronouns';
+    case 'Artikel':
+      return 'https://en.wikibooks.org/wiki/German/Grammar/Noun_phrases#Articles_in_German';
+    case 'Adverbiale':
+    case 'Adverb':
+    case 'Adjektiv':
+      return 'https://en.wikibooks.org/wiki/German/Grammar/Adjectives_and_Adverbs';
+    default:
+      return;
+  }
 }
 
 app.listen(port, () => {
